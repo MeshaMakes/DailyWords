4da6b1d817418cee43691e15c44f144c7172c883
MeshaMakes <meshamakesthings@gmail.com>
2021-10-04 11:24:14 -0400

    added footer, minor fixes

--- dailywords/src/App.js
+++ dailywords/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Definition from './components/Definition/Definition';
 import WordGuess from './components/WordGuess/WordGuess';
 import AddGuess from './components/AddGuess/AddGuess';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       <Definition def={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, deserunt sit? Impedit porro, illo odio dolore enim at, delectus eius qui consequatur tenetur sed obcaecati autem illum fuga deleniti veniam!"}/>
       <WordGuess />
       <AddGuess />
+      
+      <Footer />
     </div>
   );
 }
---
+++ dailywords/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+import {ReactComponent as Light} from '../../Icons/light.svg'
+
+const Footer = () => {
+    return (
+        <div className="footerWrap">
+            <button>Give Up?</button>
+            <Light />
+        </div>
+    )
+}
+
+export default Footer
